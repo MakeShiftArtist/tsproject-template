522a9c59539ee5a5252ff9528beb2a8d9037b250
MakeShiftArtist <lrmcbride4@gmail.com>
2022-05-24 23:28:37 -0500

    Fix: Removed tests from type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
-declare module "src/add" {
+declare module "add" {
     export function add(a: string, b: string): string;
     export function add(a: number, b: number): number;
     export default function add(a: any, b: any): any;
 }
-declare module "tests/add.test" { }
