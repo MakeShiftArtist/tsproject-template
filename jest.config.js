5d35a8e573c3eb9688cae2231e6e03608d856291
MakeShiftArtist <lrmcbride4@gmail.com>
2022-05-26 23:30:52 -0500

    Fix: Removed unneeded imports from jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./tsconfig");
-
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	roots: ["<rootDir>"],
-	modulePaths: ["<rootDir>"],
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
